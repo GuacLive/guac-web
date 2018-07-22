3b15ee8ca01421a7b0adfc9b2b76feba4fcd8c67
Thomas Lekanger <datagutt@lekanger.no>
2018-07-22 02:44:29 +0200

    Add working nonces

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,17 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
+	static getInitialProps({req, renderPage}) {
+		const { html, head, errorHtml, chunks } = renderPage()
+		return { html, head, errorHtml, chunks, 'nonce': req.nonce }
+
+	}
 	render() {
-		const {buildManifest, pathname} = this.props
+		const {buildManifest, pathname, nonce} = this.props
 		const {css} = buildManifest
 		return (
 		<html data-cast-api-enabled="true">
-			<Head nonce="potato">
+			<Head nonce={this.props.nonce}>
 			{css.map((file) => {
 				return <link rel="stylesheet" href={`/_next/${file}`} key={file} />
 			})}
@@ -15,7 +20,7 @@ export default class MyDocument extends Document {
 			</Head>
 			<body className="sans-serif h-100 w-100">
 				<Main />
-				<NextScript nonce="potato" />
+				<NextScript nonce={this.props.nonce} />
 			</body>
 		</html>
 		)
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const Koa = require('koa')
+const express = require('express')
 const next = require('next')
-const Router = require('koa-router')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -11,30 +10,24 @@ const uuidv4 = require('uuid/v4')
 
 app.prepare()
 .then(() => {
-    const server = new Koa()
-    const router = new Router()
+    const server = express()
 
-	server.use(async (ctx, next) => {
-		let nonce = /*uuidv4()*/'potato';
+	server.use((req, res, next) => {
+		let nonce = uuidv4();
 
 		let csp = `default-src 'self' guac.live *.guac.live localhost:* wss://chat.guac.live; base-uri 'self'; script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-inline' 'unsafe-eval' www.google.com www.googletagmanager.com www.google-analytics.com www.gstatic.com *.googleapis.com guac.live *.guac.live localhost:* wss://chat.guac.live; child-src www.google.com guac.live *.guac.live localhost:* wss://chat.guac.live; style-src 'self' 'unsafe-inline' *.googleapis.com guac.live *.guac.live localhost:* cdnjs.cloudflare.com; img-src 'self' data: guac.live *.guac.live wss://chat.guac.live *.googleapis.com *.gstatic.com www.google-analytics.com; media-src 'self' guac.live *.guac.live localhost:* wss://chat.guac.live; connect-src 'self' guac.live *.guac.live localhost:* wss://chat.guac.live wss://guac.live www.google-analytics.com; font-src 'self' guac.live *.guac.live *.gstatic.com data: cdnjs.cloudflare.com; object-src 'none';`;
-		ctx.res.setHeader('content-security-policy', csp);
-		ctx.res.statusCode = 200
-		ctx.state.nonce = nonce;
-		await next()
+		res.setHeader('content-security-policy', csp);
+		req.nonce = nonce;
+		next();
 	})
 
-	router.get('/c/:name', async ctx => {
-		await app.render(ctx.req, ctx.res, '/c', ctx.params)
-		ctx.respond = false
+	server.get('/c/:name', (req, res) => {
+		return app.render(req, res, '/c', req.query)
 	})
 
-	router.get('*', async ctx => {
-		await handle(ctx.req, ctx.res)
-		ctx.respond = false
-	})
-
-    server.use(router.routes())
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
 
 	server.listen(port, (err) => {
 		if (err) throw err
