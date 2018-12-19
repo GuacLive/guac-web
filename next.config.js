fd503d74a24d982b299b0aea0c63a169776390db
Thomas Lekanger <datagutt@lekanger.no>
2018-12-19 22:32:36 +0100

    add gdpr shit

--- next.config.js
+++ next.config.js
@@ -21,7 +21,30 @@ module.exports = withCSS({
 		mySecret: 'secret'
 	},
 	publicRuntimeConfig: { // Will be available on both server and client
-		API_URL: process.env.API_URL || 'http://api.local.guac.live'
+		API_URL: process.env.API_URL || 'http://api.local.guac.live',
+		OIL_CONFIG: {
+			"theme": "dark",
+			"cpc_type": "standard",
+			"publicPath": "//privacy.guac.live/scripts/",
+			"poi_activate_poi": true,
+			"poi_hub_origin": "//privacy.guac.live",
+			"poi_hub_path": "/hub.html",
+			"poi_subscriber_set_cookie": true,
+			"poi_group_name": "guac.live",
+			"timeout": -1,
+			"default_to_optin": false,
+			"advanced_settings": true,
+			"advanced_settings_purposes_default": true,
+			"locale": {
+				"localeId": "enEN_01",
+				"version": 1,
+				"texts": {
+					"label_intro_heading": "guac.live",
+					"label_intro": "This site uses cookies. By continuing to use this site, closing this banner, or clicking \"I Agree\", you agree to the use of cookies. Read our <a href=\"/privacy\" target=\"_blank\">privacy statement</a> for more information.",
+					"label_button_yes": "I agree"
+				}
+			}
+		}
 	},
 	postcssLoaderOptions: {}
 })
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,9 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
+import getConfig from 'next/config'
+const { publicRuntimeConfig } = getConfig()
+const { OIL_CONFIG } = publicRuntimeConfig
+
 export default class MyDocument extends Document {
 	static getInitialProps({req, renderPage}) {
 		const { html, head, errorHtml, chunks } = renderPage()
@@ -7,13 +11,16 @@ export default class MyDocument extends Document {
 
 	}
 	render() {
-		const {buildManifest, pathname, nonce} = this.props
-		const {css} = buildManifest
+		const {nonce} = this.props;
+		let oil = `/*! 1.2.5-SNAPSHOT */!function(e){var n={};function t(o){if(n[o])return n[o].exports;var r=n[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,t),r.l=!0,r.exports}t.m=e,t.c=n,t.d=function(e,n,o){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:o})},t.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="/",t(t.s=115)}({115:function(e,n,t){"use strict";!function(e,n){e.__cmp||(e.__cmp=function(){function t(e){if(e){var t=!0,r=n.querySelector('script[type="application/configuration"]#oil-configuration');if(null!==r&&r.text)try{var a=JSON.parse(r.text);a&&a.hasOwnProperty("gdpr_applies_globally")&&(t=a.gdpr_applies_globally)}catch(e){}e({gdprAppliesGlobally:t,cmpLoaded:o()},!0)}}function o(){return!(!e.AS_OIL||!e.AS_OIL.commandCollectionExecutor)}!function e(){if(!(n.getElementsByName("__cmpLocator").length>0))if(n.body){var t=n.createElement("iframe");t.style.display="none",t.name="__cmpLocator",n.body.appendChild(t)}else setTimeout(e,5)}();var r=[],a=function(n,a,c){if("ping"===n)t(c);else{var i={command:n,parameter:a,callback:c};r.push(i),o()&&e.AS_OIL.commandCollectionExecutor(i)}};return a.commandCollection=r,a.receiveMessage=function(n){var a=n&&n.data,c="string"==typeof a;if(a=c&&-1!==a.indexOf("__cmpCall")?JSON.parse(a).__cmpCall:a.__cmpCall)if("ping"===a.command)t(function(e,t){var o={__cmpReturn:{returnValue:e,success:t,callId:a.callId}};n.source.postMessage(c?JSON.stringify(o):o,n.origin)});else{var i={callId:a.callId,command:a.command,parameter:a.parameter,event:n};r.push(i),o()&&e.AS_OIL.commandCollectionExecutor(i)}},function(n){var t=e.addEventListener||e.attachEvent;t("attachEvent"===t?"onmessage":"message",function(e){n.receiveMessage(e)},!1)}(a),a}())}(window,document)}});`;
 		return (
 		<html data-cast-api-enabled="true">
 			<Head nonce={this.props.nonce}>
-			<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" />
-			<script type="text/javascript" src="//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1" nonce={this.props.nonce}></script>
+				<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" />
+				<script type="text/javascript" src="//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1" nonce={this.props.nonce}></script>
+				<script type="application/configuration" id="oil-configuration" dangerouslySetInnerHTML={{__html: JSON.stringify(OIL_CONFIG)}}></script>
+				<script type="text/javascript" dangerouslySetInnerHTML={{__html: oil}} nonce={this.props.nonce}></script>
+				<script type="text/javascript" src="//privacy.guac.live/scripts/oil.1.2.5-RELEASE.min.js" nonce={this.props.nonce}></script>
 			</Head>
 			<body className="sans-serif h-100 w-100">
 				<Main />
