6982fec96de55b626a147848ad42e1e57a956d72
datagutt <datagutt@lekanger.no>
2019-04-03 19:57:20 +0200

    GuacButton: Use next/link

--- components/GuacButton/index.js
+++ components/GuacButton/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'
 
 import classNames from 'classnames'
 export default class extends React.Component {
@@ -8,7 +9,7 @@ export default class extends React.Component {
 		// dib pv2 ph3 nowrap lh-solid pointer br2 ba b--transparent
 		const buttonClass = classNames(`link color-inherit dib pv2 ph3 nowrap lh-solid pointer br2 ba b--${this.props.border || this.props.color || 'transparent'} bg-${this.props.color || 'transparent'}`)
 		return this.props.children ? (
-			<a href={this.props.url || '#'} className={buttonClass} children={this.props.children} />
+			<Link href={this.props.url || '#'}><a className={buttonClass} children={this.props.children} /></Link>
 		) : null
 	}
 }
