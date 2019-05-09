b029f75ea381addb6dfe25830f0976a5930a528b
Thomas Lekanger <datagutt@lekanger.no>
2019-05-10 01:14:38 +0200

    Menu: Dark mode toggle

---
+++ components/DarkModeToggle/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Switch from 'react-switch';
+
+import {connect} from 'react-redux';
+import { setCookie } from '../../utils/cookie';
+class DarkModeToggle extends Component {
+  constructor(props) {
+    super();
+    this.state = { checked: props.site.mode === 'dark' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(checked) {
+    this.setState({ checked });
+    this.props.dispatch({
+        type: checked ? 'SET_DARK_MODE' : 'SET_LIGHT_MODE'
+    })
+    setCookie('site-mode', checked ? 'dark' : 'light');
+  }
+
+  render() {
+    return (
+      <label>
+        <Switch 
+        onChange={this.handleChange} 
+        checked={this.state.checked}
+        offColor="#212121"
+        onColor="#F1F1F1"
+        onHandleColor="#212121"
+        offHandleColor="#F1F1F1"
+        uncheckedIcon={<span>☀️</span>}
+        checkedIcon={<span>🌙</span>}
+         />
+      </label>
+    );
+  }
+}
+export default connect(state => state)(DarkModeToggle);
