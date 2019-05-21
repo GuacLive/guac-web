e99a2da7fd4867abb63658415ee359af026a0bdb
datagutt <datagutt@lekanger.no>
2019-05-21 03:36:54 +0200

    Chat: Convert EmojiSelector and GifSelector to hooks

--- components/Chat/EmojiSelector.js
+++ components/Chat/EmojiSelector.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 import { NimblePicker } from 'emoji-mart';
 import 'emoji-mart/css/emoji-mart.css';
@@ -7,69 +6,45 @@ import twitterData from 'emoji-mart/data/twitter.json';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class EmojiSelector extends React.PureComponent {
-	state = {
-		isOpen: false,
-		emotes: []
+function EmojiSelector(props){
+	const [isOpen, setIsOpen] = useState(false);
+	const [emotes, setEmotes] = useState(props.emotes);
+
+	const handleToggleClick = () => {
+		setIsOpen(!isOpen);
 	};
 
-	static propTypes = {
-		/**
-		 * Callback to be called on icon select
-		 */
-		onSelect: PropTypes.func.isRequired
-	};
-  
-	constructor(props){
-		super(props);
-		this.state = {
-			emotes: props.emotes
-		};
-		this.handleToggleClick = this.handleToggleClick.bind(this);
-	}
-	
-	/**
-	 * Handles open/close state
-	 */
-	handleToggleClick() {
-		this.setState({
-			isOpen: !this.state.isOpen
-		});
-	};
-
-	render() {
-		const { isOpen, emotes } = this.state;
-		return (
-			<div className="chat-input__buttons__emote inline-flex items-center justify-center mr2">
-				<FontAwesomeIcon icon='smile-wink' onClick={this.handleToggleClick} />
-				<span className="absolute right-0 fr bottom-2 pv5">
-					{isOpen &&
-						<NimblePicker
-							perLine={8}
-							emojiSize={24}
-							sheetSize={20}
-							set="twitter"
-							custom={emotes}
-							onSelect={this.props.onSelect}
-							data={twitterData}
-							include={[
-								'recent',
-								'custom',
-								'people',
-								'nature',
-								'foods',
-								'activity',
-								'places',
-								'objects',
-								'symbols',
-								'flags',
-							]}
-							showPreview={true}
-						/>
-					}
-				</span>
-			</div>
-		);
-	}
+	return (
+		<div className="chat-input__buttons__emote inline-flex items-center justify-center mr2">
+			<FontAwesomeIcon icon='smile-wink' onClick={handleToggleClick} />
+			<span className="absolute right-0 fr bottom-2 pv5">
+				{isOpen &&
+					<NimblePicker
+						perLine={8}
+						emojiSize={24}
+						sheetSize={20}
+						set="twitter"
+						custom={emotes}
+						onSelect={props.onSelect}
+						data={twitterData}
+						include={[
+							'recent',
+							'custom',
+							'people',
+							'nature',
+							'foods',
+							'activity',
+							'places',
+							'objects',
+							'symbols',
+							'flags',
+						]}
+						showPreview={true}
+					/>
+				}
+			</span>
+		</div>
+	);
 }
+
 export default EmojiSelector;
--- components/Chat/GifSelector.js
+++ components/Chat/GifSelector.js
@@ -1,57 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 import GiphySelect from 'react-giphy-select';
 import 'react-giphy-select/lib/styles.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class GifSelector extends React.PureComponent {
-	state = {
-		isOpen: false
+function GifSelector(props){
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleToggleClick = () => {
+		setIsOpen(!isOpen);
 	};
 
-	static propTypes = {
-		/**
-		 * Callback to be called on icon select
-		 */
-		onEntrySelect: PropTypes.func.isRequired
-	};
-  
-	constructor(props){
-		super(props);
-		this.state = {
-		};
-		this.handleToggleClick = this.handleToggleClick.bind(this);
-	}
-	
-	/**
-	 * Handles open/close state
-	 */
-	handleToggleClick() {
-		this.setState({
-			isOpen: !this.state.isOpen
-		});
-	};
-
-	render() {
-		const { isOpen, emotes } = this.state;
-		return (
-			<div className="chat-input__buttons__gif inline-flex items-center justify-center mr2">
-				<FontAwesomeIcon icon='video' onClick={this.handleToggleClick} />
-				<span className="absolute right-0 fr bottom-2 pv5">
-					{isOpen &&
-                        <GiphySelect
-                        	theme={{
-                        		select: `pa1 ba b--gray br2 bg-white near-black`
-                        	}}
-							requestKey={process.env.GIPHY_API_KEY}
-							onEntrySelect={this.props.onEntrySelect}
-                        />
-					}
-				</span>
-			</div>
-		);
-	}
+	return (
+		<div className="chat-input__buttons__gif inline-flex items-center justify-center mr2">
+			<FontAwesomeIcon icon='video' onClick={handleToggleClick} />
+			<span className="absolute right-0 fr bottom-2 pv5">
+				{isOpen &&
+					<GiphySelect
+					theme={{
+						select: `pa1 ba b--gray br2 bg-white near-black`
+					}}
+					requestKey={process.env.GIPHY_API_KEY}
+					onEntrySelect={props.onEntrySelect}
+					/>
+				}
+			</span>
+		</div>
+	);
 }
+
 export default GifSelector;
