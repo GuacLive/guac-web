6dc3234a9a2a07c091b51e3ea28c72381ffb306a
Thomas Lekanger <datagutt@lekanger.no>
2019-08-26 11:46:52 +0200

    Create channels.js

---
+++ pages/channels.js
@@ -0,0 +1,69 @@
+import React, {Component, Fragment} from 'react';
+
+import dynamic from 'next/dynamic';
+
+import {connect} from 'react-redux';
+
+import * as actions from '../actions';
+
+import { Trans } from '@lingui/macro';
+
+import Link from 'next/link';
+
+class ChannelsPage extends Component {
+	static async getInitialProps({store}){
+		const { channels } = store.getState()
+		if(channels.loading){
+			await store.dispatch(actions.fetchChannels());
+		}
+		return {...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})};
+    }
+
+	render() {
+		const { channels } = this.props;
+		if(channels.loading) return null;
+		return (
+			<Fragment>
+				<div className="w-100">
+					<h2 className="f2 tracked mt0 mb3"><Trans>Channels</Trans></h2>
+                    <div className="site-component-channels">
+                        {channels.map((channel) => {
+                            return (
+                                <div className="site-component-channels__channel">
+                                      <article className="hide-child relative ba b--black-20 mw5 center">
+                                        <Image src={channel.thumbnail} shape="rounded" fit="contain"></Image>
+                                        <div className="pa2">
+                                            {channel.user.name}
+                                            <a className="f6 db link dark-green hover-green">
+                                                <Link href={`/c/${channel.user.name}`}>
+                                                    <a className="link inherit">{channel.title}</a>
+                                                </Link>
+                                            </a>
+                                            <span className="f6 gray mv1">
+                                                <Link href={`/c/${channel.user.name}`}>
+                                                    <a className="link inherit">{channel.user.name}</a>
+                                                </Link>
+                                            </span>
+                                            <span className="f6 gray mv1">
+                                                <p>
+                                                    playing
+                                                    <Link href={`/category/${channel.category}`}>
+                                                        <a className="link inherit b">{channel.category}</a>
+                                                    </Link>
+                                                </p>
+                                            </span>
+                                            <Link href={`/c/${channel.user.name}`}>
+                                                <a className="link tc ph3 pv1 db bg-animate bg-dark-green hover-bg-green white f6 br1">Watch</a>
+                                            </Link>
+                                        </div>
+                                    </article>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+			</Fragment>
+		)
+	}
+}
+export default connect(state => state)(ChannelsPage)
