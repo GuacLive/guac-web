1d0aa787212ad1501130cb1942a1e6ab1533b7a2
datagutt <datagutt@lekanger.no>
2019-09-26 16:57:30 +0200

    Revert "Revert "Update auth.js""
    This reverts commit 01d5048be8917efed41c895f0c446bb0ab7afb5a.

--- actions/auth.js
+++ actions/auth.js
@@ -40,7 +40,7 @@ export const register = (username, password) => async (dispatch) => {
 	dispatch({
 		type: 'AUTHENTICATE_REQUEST'
 	})
-	return callApi('/auth/register', {
+	return callApi('/register', {
 		method: 'POST',
 		body: JSON.stringify({
 			username,
