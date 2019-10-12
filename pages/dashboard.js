e9b265d01b664ed2b97c0957545dd2f01ab59cff
Thomas Lekanger <datagutt@lekanger.no>
2019-10-13 01:40:31 +0200

    Dashboard: Update streaming server

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -102,8 +102,9 @@ class DashboardPage extends Component {
 						<li>
 							<p>First, choose the streaming server closest to you:</p>
 							<ul className="list">
-								<li><b>Oslo, Europe:</b> rtmp://osl1.stream.guac.live:1935/live</li>
-								<li><b>London, Europe:</b> rtmp://lon1.stream.guac.live:1935/live</li>
+								<li><b>London, Europe:</b> rtmp://stream.guac.live:1935/live</li>
+								{/*<li><b>Oslo, Europe:</b> rtmp://osl1.stream.guac.live:1935/live</li>
+								<li><b>London, Europe:</b> rtmp://lon1.stream.guac.live:1935/live</li>*/}
 							</ul>
 						</li>
 						<li>
