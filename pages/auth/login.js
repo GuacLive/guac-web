d7e5dd2b54a6849513e1b9197ddf55b10e0863d2
Thomas Lekanger <datagutt@lekanger.no>
2019-10-30 11:54:44 +0100

    Fix auth forms in dark mode

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -52,16 +52,16 @@ class LoginPage extends Component {
 							}
 							<div className="mt3">
 								<label className="db fw6 lh-copy f6" htmlFor="username"><Trans>Username</Trans></label>
-								<input ref="username" className="pa2 input-reset ba b--inherit bg-transparent hover-bg-green hover-white w-100" type="text" name="username"  id="username" />
+								<input ref="username" className="pa2 input-reset ba b--inherit primary bg-transparent hover-bg-green hover-white w-100" type="text" name="username"  id="username" />
 							</div>
 							<div className="mv3">
 								<label className="db fw6 lh-copy f6" htmlFor="password"><Trans>Password</Trans></label>
-								<input ref="password" className="b pa2 input-reset ba b--inherit bg-transparent hover-bg-green hover-white w-100" type="password" name="password"  id="password" />
+								<input ref="password" className="b pa2 input-reset ba b--inherit primary bg-transparent hover-bg-green hover-white w-100" type="password" name="password"  id="password" />
 							</div>
 							<label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> <Trans>Remember me</Trans></label>
 						</fieldset>
 						<div className="">
-							<input className="b ph3 pv2 input-reset color-inherit ba b--inherit bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+							<input className="b ph3 pv2 input-reset color-inherit ba b--inherit primary bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
 						</div>
 						<div className="lh-copy mt3">
 							<Link prefetch href="/auth/register"><a className="f6 link dim primary db"><Trans>Sign up</Trans></a></Link>
--- pages/auth/register.js
+++ pages/auth/register.js
@@ -52,15 +52,15 @@ class RegisterPage extends Component {
 							}
 							<div className="mt3">
 								<label className="db fw6 lh-copy f6" htmlFor="username"><Trans>Username</Trans></label>
-								<input ref="username" className="pa2 input-reset ba b--inherit bg-transparent hover-bg-green hover-white w-100" type="text" name="username"  id="username" />
+								<input ref="username" className="pa2 input-reset ba b--inherit primary bg-transparent hover-bg-green hover-white w-100" type="text" name="username"  id="username" />
 							</div>
 							<div className="mv3">
 								<label className="db fw6 lh-copy f6" htmlFor="password"><Trans>Password</Trans></label>
-								<input ref="password" className="b pa2 input-reset ba b--inherit bg-transparent hover-bg-green hover-white w-100" type="password" name="password"  id="password" />
+								<input ref="password" className="b pa2 input-reset ba b--inherit primary bg-transparent hover-bg-green hover-white w-100" type="password" name="password"  id="password" />
 							</div>
 						</fieldset>
 						<div className="">
-							<input className="b ph3 pv2 input-reset color-inherit ba b--inherit bg-transparent grow pointer f6 dib" type="submit" value="Register" />
+							<input className="b ph3 pv2 input-reset color-inherit ba b--inherit primary bg-transparent grow pointer f6 dib" type="submit" value="Register" />
 						</div>
 						<div className="lh-copy mt3">
 							<Link prefetch href="/auth/login"><a className="f6 link dim primary db"><Trans>Log in</Trans></a></Link>
