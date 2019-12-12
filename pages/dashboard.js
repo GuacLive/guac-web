ab9211b6a9cf60ca6df8cde4037e033f4f8e7890
datagutt <datagutt@lekanger.no>
2019-12-12 16:12:22 +0100

    Dashboard: New design

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux';
+import dynamic from 'next/dynamic'
 
 import withAuth from '../utils/withAuth';
 import * as actions from '../actions';
 
+
+let VideoPlayer = dynamic(
+	() => /* webpackChunkName: 'VideoPlayer' */import('../components/VideoPlayer'),
+	{
+		ssr: false,
+		loading: () => <div className="w-100 h-100 bg-black white content-box" style={{'paddingTop': '56.25%'}} />
+	}
+);
 class DashboardPage extends Component {
 	constructor(props){
 		super(props);
@@ -11,10 +20,13 @@ class DashboardPage extends Component {
 	}
 
 	static async getInitialProps({store, isServer, pathname, query, req}){
-		const { streaming, authentication } = store.getState()
+		const { streaming, channel, authentication } = store.getState()
 		if(streaming.loading){
 			await store.dispatch(actions.fetchStreaming(authentication.token));
 			await store.dispatch(actions.fetchCategories(authentication.token));
+		}
+		if(channel.loading){
+			await store.dispatch(actions.fetchChannel(authentication.user.name));
 		}
     }
 
@@ -37,26 +49,76 @@ class DashboardPage extends Component {
 				actions.setTitle(this.props.authentication.token, this.refs.title.value)
 			);
 		}
-			console.log(this.refs.private, this.refs.private.checked);
 		if(streaming.private !== this.refs.private.checked){
 			this.props.dispatch(
 				actions.setPrivate(this.props.authentication.token, this.refs.private.checked)
 			);
 		}
 	}
+    renderStream(){
+		const {
+			channel
+		} = this.props;
+		let stream = channel.data;
+
+		let videoJsOptions = { 
+			autoplay: false,
+			controls: true,
+			sources: [],
+			streamInfo: {
+				username: stream.user.name
+			}
+		};
+
+		if(stream.live){
+			if(stream.urls){
+				// Prefer FLV if available, it has lower latency
+				let flvUrl = stream.servers[STREAMING_SERVER] + stream.urls.flv;
+				if(stream.urls.flv){
+					videoJsOptions.sources.push({
+						src: typeof window === 'object' && 'WebSocket' in window
+							? `${window.location.protocol === 'https:' ? 'wss' : 'ws'}:${flvUrl}`
+							: flvUrl,
+						type: 'video/x-flv',
+						label: STREAMING_SERVER + `(FLV)`
+					});
+				}
+				// Only HLS has quality options
+				Object.keys(stream.qualities).forEach((key) => {
+					let urlKey = stream.qualities[key];
+					videoJsOptions.sources.push({
+						src: stream.servers[STREAMING_SERVER] + `/live/${stream.user.name}/index${urlKey}.m3u8`,
+						type: 'application/x-mpegURL',
+						label: STREAMING_SERVER + `(${key})`
+					});
+				});
+			}
+		}
+
+    	return (
+			<div class="w-100 h-100 flex flex-column flex-grow-1 overflow-hidden relative">
+                <VideoPlayer { ...videoJsOptions } live={stream.live}></VideoPlayer>
+            </div>
+		);
+	}
 
 	render(){
-		const {streaming, categories} = this.props;
+		const {streaming, categories, channel} = this.props;
 		const auth = this.props.authentication;
 		if(auth.loading) return null;
 		if(auth.error) throw auth.error;
 		if(streaming.loading) return null;
+		if(channel.loading) return null;
 		if(categories.error) throw categories.error;
 		if(streaming.error) throw streaming.error;
 		if(auth && auth.user && !auth.user.can_stream) return <p>You do not have permission to stream</p>;
 		return (
-			<>
-				<div className="w-100">
+			<div className="flex flex-row flex-wrap w-100">
+				<div className="w-50 pa3">
+					<h2 className="f2 tracked mt0 mb3">Stream preview</h2>
+					{ this.renderStream() }
+				</div>
+				<div className="w-50 pa3">
 					<h2 className="f2 tracked mt0 mb3">Stream settings</h2>
 					<form className="measure" onSubmit={this.handleSubmit}>
 						<label htmlFor="title">Title:</label>
@@ -98,6 +160,8 @@ class DashboardPage extends Component {
 						</input>
 						<input type="submit" value="Edit stream" className="link color-inherit dib pv2 ph3 nowrap lh-solid pointer br2 ba b--green bg-green ml1" />
 					</form>
+				</div>
+				<div className="w-50 pa3">
 					<h2 className="f2 tracked mt0 mb3">Get started with streaming</h2>
 					<ol>
 						<li>
@@ -115,6 +179,8 @@ class DashboardPage extends Component {
 							<p>At last, make sure keyframe interval is set to <b>2</b>.</p>
 						</li>
 					</ol>
+				</div>
+				<div className="w-50 pa3">
 					<h2 className="f2 tracked mt0 mb3">Chat commands:</h2>
 					<ul>
 						<li><b>/help</b> - Show help command</li>
@@ -127,7 +193,7 @@ class DashboardPage extends Component {
 						<li><b>/unban <i>user</i></b> - Unban user</li>
 					</ul>
 				</div>
-			</>
+			</div>
 		)
 	}
 }
