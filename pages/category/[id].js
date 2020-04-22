1208a012466fb3e61e0777cd9443c89252dead97
datagutt <datagutt@lekanger.no>
2020-04-22 19:51:11 +0200

    Update [id].js

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -36,7 +36,7 @@ class CategoryPage extends Component {
 		console.log(thisCategory);
 		return (
 			<Fragment>
-				<div className="w-100">
+				<div className="w-100 pv3 ph3-l">
 					<h2 className="f2 tracked mt0 mb3">
 					{
 						thisCategory
