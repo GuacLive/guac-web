014e2a63425636f2e48b7a1556387f4cdcb4442f
datagutt <datagutt@lekanger.no>
2020-05-08 16:17:38 +0200

    Replace OIL with Quantcast Choice

--- pages/_document.js
+++ pages/_document.js
@@ -38,10 +38,10 @@ export default class MyDocument extends Document {
 
 	render() {
 		const {nonce} = this.props;
-		let oil = `!function(e){var n={};function t(o){if(n[o])return n[o].exports;var r=n[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,t),r.l=!0,r.exports}t.m=e,t.c=n,t.d=function(e,n,o){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:o})},t.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="/",t(t.s=115)}({115:function(e,n,t){"use strict";!function(e,n){e.__cmp||(e.__cmp=function(){function t(e){if(e){var t=!0,r=n.querySelector('script[type="application/configuration"]#oil-configuration');if(null!==r&&r.text)try{var a=JSON.parse(r.text);a&&a.hasOwnProperty("gdpr_applies_globally")&&(t=a.gdpr_applies_globally)}catch(e){}e({gdprAppliesGlobally:t,cmpLoaded:o()},!0)}}function o(){return!(!e.AS_OIL||!e.AS_OIL.commandCollectionExecutor)}var r=[],a=function(n,a,c){if("ping"===n)t(c);else{var i={command:n,parameter:a,callback:c};r.push(i),o()&&e.AS_OIL.commandCollectionExecutor(i)}};return a.commandCollection=r,a.receiveMessage=function(n){var a=n&&n.data&&n.data.__cmpCall;if(a)if("ping"===a.command)t(function(e,t){var o={__cmpReturn:{returnValue:e,success:t,callId:a.callId}};n.source.postMessage(o,n.origin)});else{var c={callId:a.callId,command:a.command,parameter:a.parameter,event:n};r.push(c),o()&&e.AS_OIL.commandCollectionExecutor(c)}},function(n){(e.attachEvent||e.addEventListener)("message",function(e){n.receiveMessage(e)},!1)}(a),function e(){if(!(n.getElementsByName("__cmpLocator").length>0))if(n.body){var t=n.createElement("iframe");t.style.display="none",t.name="__cmpLocator",n.body.appendChild(t)}else setTimeout(e,5)}(),a}())}(window,document)}});`;
+		let quant = `!function(){var e=window.location.hostname,t=document.createElement("script"),a=document.getElementsByTagName("script")[0],n=(new Date).getTime(),c="https://quantcast.mgr.consensu.org".concat("/choice/","ApXc11SvmTDme","/",e,"/choice.js").concat("?timestamp=",n);function o(){var e,c="__tcfapiLocator",n=[],o=window;for(;o;){try{if(o.frames[c]){e=o;break}}catch(e){}if(o===window.top)break;o=o.parent}e||(!function e(){var t=o.document,a=!!o.frames[c];if(!a)if(t.body){var n=t.createElement("iframe");n.style.cssText="display:none",n.name=c,t.body.appendChild(n)}else setTimeout(e,5);return!a}(),o.__tcfapi=function(){var e,t=arguments;if(!t.length)return n;if("setGdprApplies"===t[0])3<t.length&&2===t[2]&&"boolean"==typeof t[3]&&(e=t[3],"function"==typeof t[2]&&t[2]("set",!0));else if("ping"===t[0]){var a={gdprApplies:e,cmpLoaded:!1,cmpStatus:"stub"};"function"==typeof t[2]&&t[2](a)}else n.push(t)},o.addEventListener("message",function(n){var c="string"==typeof n.data,e={};try{e=c?JSON.parse(n.data):n.data}catch(e){}var o=e.__tcfapiCall;o&&window.__tcfapi(o.command,o.version,function(e,t){var a={__tcfapiReturn:{returnValue:e,success:t,callId:o.callId}};c&&(a=JSON.stringify(a)),n.source.postMessage(a,"*")},o.parameter)},!1))}t.async=!0,t.type="text/javascript",t.src=c,a.parentNode.insertBefore(t,a),"undefined"!=typeof module?module.exports=o:o()}();`;
 		let darkMode = 'function prefersDarkMode(){var e=window.CSS&&window.CSS.supports.bind(window.CSS)||window.supportsCSS;return!!(!!e&&(e("--f:0")||e("--f",0)))&&(window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches)}-1!==document.cookie.indexOf("site-mode=dark"),("dark"===localStorage.getItem("site_mode")||prefersDarkMode())&&document.querySelector("html").classList.replace("guac-skin-light","guac-skin-dark")';
 		let event = 'window.sa_event=window.sa_event||function(){a=[].slice.call(arguments);sa_event.q?sa_event.q.push(a):sa_event.q=[a]};';
-		let perf = 'hydrationMetrics&&hydrationMetrics.onInputDelay&&performance&&hydrationMetrics.onInputDelay(function(e,n){sa_event("event",{eventCategory:"Perf Metrics",eventAction:"first-input-delay",eventLabel:n.type,eventValue:Math.round(e),nonInteraction:!0})});';
+		//let perf = 'hydrationMetrics&&hydrationMetrics.onInputDelay&&performance&&hydrationMetrics.onInputDelay(function(e,n){sa_event("event",{eventCategory:"Perf Metrics",eventAction:"first-input-delay",eventLabel:n.type,eventValue:Math.round(e),nonInteraction:!0})});';
 		return (
 			<html lang={this.props.locale} data-cast-api-enabled="true" id="guac" className={this.props.mode === 'dark' ? 'guac-skin-dark' : 'guac-skin-light'}>
 				<MyHead nonce={nonce}>
@@ -65,8 +65,7 @@ export default class MyDocument extends Document {
 					<meta name="apple-touch-fullscreen" content="yes" />
 					<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
-					<script type="application/configuration" id="oil-configuration" dangerouslySetInnerHTML={{__html: JSON.stringify(process.env.OIL_CONFIG)}}></script>
-					<script type="text/javascript" dangerouslySetInnerHTML={{__html: oil}} nonce={nonce}></script>
+ 					<script type="text/javascript" dangerouslySetInnerHTML={{__html: quant}} nonce={nonce}></script>
 					<script type="text/javascript" dangerouslySetInnerHTML={{__html: event}} nonce={nonce}></script>
 					<script type="text/javascript" dangerouslySetInnerHTML={{__html: darkMode}} nonce={nonce}></script>
 				</MyHead>
@@ -75,7 +74,6 @@ export default class MyDocument extends Document {
 					<NextScript nonce={nonce} />
 					<script async defer src="https://cheese.guac.live/app.js" nonce={nonce}></script>
 					<noscript><img src="https://cheese.guac.live/image.gif" alt="" /></noscript>
-					<script type="text/javascript" dangerouslySetInnerHTML={{__html: perf}} nonce={nonce}></script>
 				</body>
 			</html>
 		)
