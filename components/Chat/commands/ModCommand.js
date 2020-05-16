6e4ae949303224e1071f7e3f8f0cd7f29907abb0
datagutt <datagutt@lekanger.no>
2020-05-16 15:42:09 +0200

    Chat: more lenient mod/unmod checks

--- components/Chat/commands/ModCommand.js
+++ components/Chat/commands/ModCommand.js
@@ -2,12 +2,13 @@ import Command from './Command';
 export default class ModCommand extends Command {
     run(args) {
         if(!this.hasPrivilege) return;
-        if(
+        // Just do this check server-side instead
+        /*if(
             this.me
             && this.channel.data.user.name !== this.me.name
         ){
             return;
-        }
+        }*/
         if(args && args[0]){
             let user = this.users.get(args[0]);
             this.socket.emit('mod', user && user.id);
