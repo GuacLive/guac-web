20154f17178667368d9c60db708edbbe532bd0ac
datagutt <datagutt@lekanger.no>
2020-05-25 16:33:27 +0200

    Reducers: add back flopflip reducer
    this seems to hold the application together, if i remove it, it breaks

--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,13 @@
 import { combineReducers } from 'redux';
 
+import {	
+	createFlopflipReducer,	
+
+	// We refer to this state slice in the `injectFeatureToggles`	
+	// HoC and currently do not support a custom state slice.	
+	FLOPFLIP_STATE_SLICE	
+} from '@flopflip/react-redux';	
+
 import auth from './auth';
 import categories from './categories';
 import channel from './channel';
@@ -12,6 +20,7 @@ import streams from './streams';
 import site from './site';
 
 export default combineReducers({
+	[FLOPFLIP_STATE_SLICE]: createFlopflipReducer(),
 	authentication: auth,
 	categories,
 	channel,
