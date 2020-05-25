e00a0bdf00518c92902a920d6ea9ae3dbf1113a7
datagutt <datagutt@lekanger.no>
2020-05-25 16:37:14 +0200

    Revert "Reducers: add back flopflip reducer"
    This reverts commit 20154f17178667368d9c60db708edbbe532bd0ac.

--- reducers/index.js
+++ reducers/index.js
@@ -1,13 +1,5 @@
 import { combineReducers } from 'redux';
 
-import {	
-	createFlopflipReducer,	
-
-	// We refer to this state slice in the `injectFeatureToggles`	
-	// HoC and currently do not support a custom state slice.	
-	FLOPFLIP_STATE_SLICE	
-} from '@flopflip/react-redux';	
-
 import auth from './auth';
 import categories from './categories';
 import channel from './channel';
@@ -20,7 +12,6 @@ import streams from './streams';
 import site from './site';
 
 export default combineReducers({
-	[FLOPFLIP_STATE_SLICE]: createFlopflipReducer(),
 	authentication: auth,
 	categories,
 	channel,
