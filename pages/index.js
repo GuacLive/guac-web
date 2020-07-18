8e51ede3f5a2a51cba52ddda5c92a20068d179a7
Thomas Lekanger <datagutt@lekanger.no>
2020-07-18 21:45:24 +0200

    new frontpage layout

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import dynamic from 'next/dynamic'
 
 import GuacButton from '../components/GuacButton'
 
-import {connect} from 'react-redux';
+import {connect, useDispatch} from 'react-redux';
 
 import * as actions from '../actions';
 
@@ -14,7 +14,7 @@ import { Trans } from '@lingui/macro'
 
 import Link from 'next/link';
 
-import Carousel from 'nuka-carousel';
+import Chat from '../components/Chat';
 
 let VideoPlayer = dynamic(
 	() => import('../components/VideoPlayer'),
@@ -23,21 +23,10 @@ let VideoPlayer = dynamic(
 		loading: () => <div className="w-100 h-100 bg-black white content-box" style={{'paddingTop': '56.25%'}} />
 	}
 );
-
 const STREAMING_SERVER = 'eu';
-class IndexPage extends Component {
-	static async getInitialProps({store}){
-		// we can dispatch from here too
-		//store.dispatch({type: 'SET_FEATURED'});
-		const { featured } = store.getState()
-		console.log('featured1', featured);
-		if(featured.loading){
-			await store.dispatch(actions.fetchFeatured());
-		}
-		return {...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})};
-    }
-
-    renderStream = stream => {
+function IndexPage(props){
+	const dispatch = useDispatch();
+    const renderStream = stream => {
 		let videoJsOptions = { 
 			autoplay: true,
 			controls: true,
@@ -82,103 +71,157 @@ class IndexPage extends Component {
 		}
 
     	return (
-    		<div key={stream.user.id}>
-    			<Link href="/c/[name]" as={`/c/${stream.name}`}>
-    				<a className="link f4 b ma0 primary">
-						<span className="i tracked b">{stream.name}</span> <Trans>is live</Trans>
-					</a>
-    			</Link>
-				<VideoPlayer { ...videoJsOptions } live={stream.live}></VideoPlayer>
+			<div key={stream.user.id} className="bg-black-50 primary flex flex-wrap justify-end w-90">
+				<div className="flex-grow-1 self-center h-100">
+					<div className="flex flex-row flex-grow-1">
+						<Link href="/c/[name]" as={`/c/${stream.name}`}>
+							<a className="link f4 b ma0 primary">
+								<span className="tracked b">{stream.name}</span>
+							</a>
+						</Link>
+					</div>
+					<VideoPlayer { ...videoJsOptions } live={stream.live} fill={true}></VideoPlayer>
+				</div>
+				<div className="flex flex-shrink-1">
+					{!channel.loading && <Chat channel={stream.name} />}
+				</div>
     		</div>
     	);
     }
     
-    renderStreams = () => {
-		if(this.props.featured.statusCode == 200
-			&& this.props.featured.data
-			&& this.props.featured.data.length > 0){
-			return (
-				<Carousel
-					width="100%"
-					initialSlideHeight={100}
-					heightMode="max"
-					className="w-100"
-					slidesToShow={6}
-					cellSpacing={20}
-					asing="easeInQuad"
-					slidesToScroll={5}
-					dragging={true}
-					wraparound={true}
-					cellAlign={'center'}
-					slideWidth={3}
-					defaultControlsConfig={{
-						nextButtonText: <i className="fa fa-arrow-right primary"></i>,
-						prevButtonText: <i className="fa fa-arrow-left primary"></i>,
-						pagingDotsStyle: {
-						  fill: 'white'
-						}
-					  }}
-				>
-					{this.props.featured.data.map(this.renderStream)}
-					</Carousel>
-			);
+    const renderTopStream = (stream) => {
+		if(stream){
+			return renderStream(stream);
 		}
 		return (
 			<Trans>no streams are online</Trans>
 		);
     }
-
-	render() {
-		const { featured } = this.props;
-		if(featured.loading) return null;
-		return (
-			<Fragment>
-				<div className="site-component-spotlight w-100 pv3 ph4-l mw9-l">
-					<h3 className="f4 b ma0 mt0 mb3 ttu tracked primary"><Trans>Live streams</Trans></h3>
-					{this.renderStreams()}
+    
+    const renderStreams = (streams) => {
+		if(streams.statusCode == 200
+			&& streams.data
+			&& streams.data.length > 0){
+			console.log('renderStreams', streams);
+			return (
+				<div className="w-100 flex flex-wrap">
+					{streams.data.map((channel) => {
+						return (
+						<div key={`featured_streams_${channel.user.id}`} className="pa2 w-third-l w-100">
+							<div className="w-100 flex flex-column bg-bar">
+								<div className="pa2 w-100 flex flex-row justify-between items-center">
+									<div className="flex items-center">
+										<div className="w3 h3 mr3 ba bw1 b--green bg-center cover br-100" style={{'backgroundImage': `url(${channel.user.avatar || '//api.guac.live/avatars/unknown.png'}`}}></div>
+										<div className="flex flex-column">
+											<Link href="/c/[name]" as={`/c/${channel.name}`}>
+												<a className="link white f4">{channel.name}</a>
+											</Link>
+											<Link href="/category/[id]" as={`/category/${channel.category_id}`}>
+												<a className="link white f5 mt2"><i className="fa fa-gamepad"></i> {channel.category_name}</a>
+											</Link>
+										</div>
+									</div>
+									<div className="flex flex-column">
+										<GuacButton color="green" url={`/c/${channel.name}`}><Trans>Watch</Trans></GuacButton>
+									</div>
+								</div>
+								<div className="w-100">
+									<div className="aspect-ratio aspect-ratio--16x9">
+										<div className="flex flex-column justify-between aspect-ratio--object bg-center cover">
+											<Link href="/c/[name]" as={`/c/${channel.name}`}>
+												<a className="link white pa2 w-100 flex justify-between f4 bg-black-70">{channel.title}</a>
+											</Link>
+											<div className="w-100 flex justify-between ph2 pt4 pb2 f5 grad-bot">
+												<span className="pv1 ph2 bg-black white br2"><i className="fa fa-circle red"></i> Live</span>
+												<span className="pv1 ph2 bg-black white br2"><i className="fa fa-eye"></i> {channel.viewers}</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						);
+					})}
 				</div>
-				<ToggleFeature
-					flag='guacWelcome'
-				>
-					<section className="ph4-l pv5">
-						<article className="center-l br2 ba b--transparent bg-black-40">
-							<div className="db w-100">
-								<div className="pa3 pa4-ns db v-mid">
-									<div>
-										<h2 className="f2 tracked mt0 mb3"><Trans>Welcome to guac.live</Trans></h2>
-										<span className="measure lh-copy mv0">
-											<Trans>
+			);
+		}
+		return null
+	}
+
+	const {channel, featured} = props;
+	if (featured.loading) return null;
+
+	/*if (channel.loading && featured.data && featured.data[0]) {
+		dispatch(actions.fetchChannel(featured.data[0].name));
+		return null;
+	}*/
+	return (
+		<Fragment>
+			<div className="site-component-spotlight justify-center flex flex-row pa2 w-100">
+				{featured.statusCode == 200
+				&& featured.data
+				&& featured.data.length > 0
+				? renderTopStream(featured.data[0]) : null
+			}
+			</div>
+			<div className="site-component-popular w-100 pa2 relative bg-black-20">
+				<h3 className="ma0 pa3"><Trans>Popular channels</Trans> <Link href="/channels"><a className="ml3 ph3 pv1 link primary-80 bg-black-30 20"><Trans>More</Trans> <small className="fa fa-chevron-right"></small></a></Link></h3>
+				{renderStreams(featured)}
+			</div>
+			<ToggleFeature
+				flag='guacWelcome'
+			>
+				<section className="ph4-l pv5">
+					<article className="center-l br2 ba b--transparent bg-black-40">
+						<div className="db w-100">
+							<div className="pa3 pa4-ns db v-mid">
+								<div>
+									<h2 className="f2 tracked mt0 mb3"><Trans>Welcome to guac.live</Trans></h2>
+									<span className="measure lh-copy mv0">
+										<Trans>
 											<p>
-											Hi,<br/>
+												Hi,<br />
 											Welcome to guac.live &mdash; live streaming platform. We are currently in beta.</p>
 											<p>
-											While we are in beta, streaming privileges will be given on a invite-only basis.
+												While we are in beta, streaming privileges will be given on a invite-only basis.
 											<br />
 											But, feel free to make an account and participate in the chat and general community!
 											</p>
-											</Trans>
-										</span>
-									</div>
-								</div>
-								{
-									(!this.props.authentication.token)
-									&&
-									<div className="pa3 pa4-l db v-mid black">
-										<GuacButton color="light-green" url="/auth/login"><Trans>Join</Trans></GuacButton>
-									</div>
-								}
-								<div className="pa3 pa4-l db">
-									<a href="https://discord.gg/k6MJSAj">
-										<img src="https://discordapp.com/api/guilds/564909420199411732/widget.png?style=banner2" alt="Discord server invite image" />
-										<span className="dn">Discord server invite</span>
-									</a>
+										</Trans>
+									</span>
 								</div>
 							</div>
-						</article>
-					</section>
-				</ToggleFeature>
-			</Fragment>
-		)
+							{
+								(!props.authentication.token)
+								&&
+								<div className="pa3 pa4-l db v-mid black">
+									<GuacButton color="light-green" url="/auth/login"><Trans>Join</Trans></GuacButton>
+								</div>
+							}
+							<div className="pa3 pa4-l db">
+								<a href="https://discord.gg/k6MJSAj">
+									<img src="https://discordapp.com/api/guilds/564909420199411732/widget.png?style=banner2" alt="Discord server invite image" />
+									<span className="dn">Discord server invite</span>
+								</a>
+							</div>
+						</div>
+					</article>
+				</section>
+			</ToggleFeature>
+		</Fragment>
+	)
+};
+IndexPage.getInitialProps = async ({store}) => {
+	// we can dispatch from here too
+	//store.dispatch({type: 'SET_FEATURED'});
+	const { channel, featured } = store.getState()
+	console.log('featured1', featured);
+	if(featured.loading){
+		await store.dispatch(actions.fetchFeatured());
 	}
+	try{
+		await store.dispatch(actions.fetchChannel(store.getState().featured.data[0].name));
+	}catch(e){}
+	return {...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})};
 }
 export default connect(state => state)(IndexPage)
