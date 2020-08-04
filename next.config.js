fc2a7909c498a418226b2afb5d8a1431392ca691
Thomas Lekanger <datagutt@lekanger.no>
2020-08-04 14:36:34 +0200

    Disable font optimization for now (breaks in vercel)

--- next.config.js
+++ next.config.js
@@ -102,7 +102,7 @@ module.exports = withTM(withOffline({
 		workerThreads: true,
 		pageEnv: true,
 		productionBrowserSourceMaps: false,
-		optimizeFonts: true,
+		optimizeFonts: false,
 		scrollRestoration: true
 	},
 	future: {
