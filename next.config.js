7832e3219e3630bf350ec703aafd4e8682f03c8e
datagutt <datagutt@lekanger.no>
2020-10-29 03:07:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -93,7 +93,7 @@ module.exports = withTM(withOffline({
 	},
 	reactStrictMode: true,
 	images: {
-		domains: ['guac.live', 'api.guac.live', 'media.rawg.io', 'stream.guac.live', '//api.guac.live', '//stream.guac.live']
+		domains: ['guac.live', 'api.guac.live', 'media.rawg.io', 'stream.guac.live', '//api.guac.live', '//stream.guac.live', 'cdn.frankerfacez.com', 'static-cdn.jtvnw.net', 'cdn.betterttv.net']
 	},
 	experimental: {
 		plugins: true,
