1fcda55f016e8b23b7eb4e8bb3892399f9c3954c
datagutt <datagutt@lekanger.no>
2020-11-26 01:15:28 +0100

    AccountMenu: Add arrow to avatar

--- components/layout/AccountMenu.js
+++ components/layout/AccountMenu.js
@@ -22,7 +22,7 @@ function AccountMenu(props){
 
 	return (
 		<div className="items-stretch flex flex-grow-1 h-100 pl2 relative">
-			<a alt="Account menu" className="db link pv2 ph3 color-inherit" href="#!" onClick={() => setIsOpen(!isOpen)}>
+			<a alt="Account menu" className="account-menu-avatar db link pv2 ph3 color-inherit" href="#!" onClick={() => setIsOpen(!isOpen)}>
 				<div className="relative w2 w2-ns h2 h2-ns">
 					<Image alt="User avatar" src={props.user && props.user.avatar || 'https://api.guac.live/avatars/unknown.png'} alt={props.user ? props.user.name : null} width={70} height={70} shape="squircle" fit="cover" lazyload className="dim" />
 				</div>
