910b4b690a86a37193bb9e482678a68e07fe1c72
datagutt <datagutt@lekanger.no>
2021-01-19 18:51:55 +0100

    Update host-meta.js

--- pages/api/well-known/host-meta.js
+++ pages/api/well-known/host-meta.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
 	const xml =  '<?xml version="1.0" encoding="UTF-8"?>\n' +
 	'<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">\n' +
-	`  <Link rel="lrdd" type="application/xrd+xml" template="${req.getHeader('Host') || 'https://guac.live/'}/.well-known/webfinger?resource={uri}"/>\n` +
+	`  <Link rel="lrdd" type="application/xrd+xml" template="${res.getHeader('Host') || 'https://guac.live/'}/.well-known/webfinger?resource={uri}"/>\n` +
 	'</XRD>';
 
 	res.statusCode = 200;
