13e66368ba63cd1b7263981d1ed9ffa2b6a9ebf5
Thomas Lekanger <datagutt@lekanger.no>
2021-02-10 20:40:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -77,7 +77,6 @@ module.exports = withTM(withOffline({
 	transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
 	// Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
 	// turn on the SW in dev mode so that we can actually test it
-	generateInDevMode: true,
 	dontAutoRegisterSw: true,
 	generateSw: false,
 	workboxOpts: {
