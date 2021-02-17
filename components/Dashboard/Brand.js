32e8f8acf0509541e3df146e1044b0ae1695bfcf
Thomas Lekanger <datagutt@lekanger.no>
2021-02-17 21:04:00 +0100

    Brand: add banner upload

--- components/Dashboard/Brand.js
+++ components/Dashboard/Brand.js
@@ -1,30 +1,35 @@
-import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux';
 
-import {useDispatch, useSelector} from 'react-redux';
+import React, {useEffect} from 'react'
 
 import { Trans } from '@lingui/macro';
 
-import * as actions from '../../actions';
+import { useDispatch, useSelector } from 'react-redux';
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import * as actions from 'actions/';
 
-import AvatarUpload from 'components/AvatarUpload';
+import dynamic from 'next/dynamic';
+const AvatarUpload = dynamic(() => import('components/AvatarUpload'));
+const BannerUpload = dynamic(() => import('components/BannerUpload'));
 
 function Brand(props){
-	const dispatch  = useDispatch();
-	
-	const {streaming, channel} = props;
-	const auth = props.authentication;
+	const dispatch = useDispatch();
+	const streaming = useSelector(state => state.streaming);
+	const auth = useSelector(state => state.authentication);
+
+	useEffect(() => {
+		dispatch(actions.fetchStreaming(auth.token));
+	}, []);
+
 	if(auth.loading) return null;
 	if(auth.error) throw auth.error;
 	if(streaming.loading) return null;
-	if(channel.loading) return null;
     if(streaming.error) throw streaming.error;
 	if(auth && auth.user && !auth.user.can_stream) return <p><Trans>You do not have permission to stream</Trans></p>;
 	return (
         <div className="flex flex-row flex-wrap w-100">
-            <AvatarUpload user={auth.user} />
+            <div className="w-100"><AvatarUpload user={auth.user} /></div>
+            <div className="w-100"><BannerUpload user={auth.user} streaming={streaming} /></div>
         </div>
 	);
 }
