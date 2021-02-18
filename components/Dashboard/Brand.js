39e95cd42aa97b24beb1bcfc4864faceb6bb4499
Thomas Lekanger <datagutt@lekanger.no>
2021-02-18 01:04:36 +0100

    BannerUpload: Try to fix current banner

--- components/Dashboard/Brand.js
+++ components/Dashboard/Brand.js
@@ -4,23 +4,13 @@ import React, {useEffect} from 'react'
 
 import { Trans } from '@lingui/macro';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import * as actions from 'actions/';
 
 import dynamic from 'next/dynamic';
 const AvatarUpload = dynamic(() => import('components/AvatarUpload'));
 const BannerUpload = dynamic(() => import('components/BannerUpload'));
 
 function Brand(props){
-	const dispatch = useDispatch();
 
-	useEffect(() => {
-		const {authentication} = props;
-		dispatch(actions.fetchStreaming(authentication.token));
-	}, []);
-	
-	const {streaming} = props;
 	const auth = props.authentication;
 	if(auth.loading) return null;
 	if(auth.error) throw auth.error;
@@ -30,7 +20,7 @@ function Brand(props){
 	return (
         <div className="flex flex-row flex-wrap w-100">
             <div className="w-100"><AvatarUpload user={auth.user} /></div>
-            <div className="w-100"><BannerUpload user={auth.user} streaming={streaming} /></div>
+            <div className="w-100"><BannerUpload user={auth.user} /></div>
         </div>
 	);
 }
