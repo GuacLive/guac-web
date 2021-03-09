c902d8303592d26da78277b72c493189ced36b0a
Thomas Lekanger <datagutt@lekanger.no>
2021-03-09 16:42:02 +0100

    Update channels.js

--- pages/channels.js
+++ pages/channels.js
@@ -11,9 +11,6 @@ import Link from 'next/link';
 import Switch from 'react-switch';
 
 import GuacButton from '../components/GuacButton';
-
-import Image from '../components/Image';
-
 class ChannelsPage extends Component {
 	_isMounted = false;
 	state = {onlyLive: true};
@@ -24,7 +21,6 @@ class ChannelsPage extends Component {
 	}
 
 	static async getInitialProps({store}) {
-		const { channels } = store.getState()
 		await store.dispatch(actions.fetchChannels(1, ''));
 		return {
 			...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})
@@ -71,7 +67,7 @@ class ChannelsPage extends Component {
 								this.state.onlyLive &&
 								<a className="link white inline-flex items-center justify-center tc pv2 ph3 nowrap lh-solid pointer br2 ba b--transparent bg-dark-gray guac-btn"
 									href="#"
-									onClick={() => this.handleChange('')}>
+									onClick={() => this.handleChange()}>
 									<Trans>View offline channels</Trans>
 								</a>
 							}
@@ -85,7 +81,7 @@ class ChannelsPage extends Component {
 									<div className="w-100 flex flex-column bg-bar">
 										<div className="pa2 w-100 flex flex-row justify-between items-center">
 											<div className="flex items-center">
-												<div className={`w3 h3 mr3 ba bw1 ${channel.live ? 'b--green' : 'b--red'} bg-center cover br-100`} style={{'backgroundImage': `url(${channel.avatar}`}}></div>
+												<div className={`w3 h3 mr3 ba bw1 ${new Boolean(+channel.live) ? 'b--green' : 'b--red'} bg-center cover br-100`} style={{'backgroundImage': `url(${channel.avatar}`}}></div>
 												<div className="flex flex-column">
 													<Link href={`/[channel]`} as={`/${channel.name}`}>
 														<a className="link white f4">{channel.name}</a>
@@ -101,13 +97,15 @@ class ChannelsPage extends Component {
 										</div>
 										<div className="w-100">
 											<div className="aspect-ratio aspect-ratio--16x9">
-												<div className="flex flex-column justify-between aspect-ratio--object bg-center cover" style={{'backgroundImage': channel.live ? `url(${channel.streamServer}/live/${channel.name}/thumbnail.jpg)` : `url(${channel.banner})`}}>
+												<div className="flex flex-column justify-between aspect-ratio--object bg-center cover" style={{'backgroundImage': new Boolean(+channel.live) ? `url(${channel.streamServer}/live/${channel.name}/thumbnail.jpg)` : `url(${channel.banner})`}}>
 													<Link href="/[channel]" as={`/${channel.name}`}>
 														<a className="link white pa2 w-100 flex justify-between f4 bg-black-70">{channel.title}</a>
 													</Link>
 													<div className="w-100 flex justify-between ph2 pt4 pb2 f5 grad-bot">
-														{channel.live ? <span className="pv1 ph2 bg-black white br2"><i className="fa fa-circle red"></i> Live</span> : <></>}
-														{channel.live ? <span className="pv1 ph2 bg-black white br2"><i className="fa fa-eye"></i> {channel.viewers}</span> : <></>}
+														{new Boolean(+channel.live) ? 
+															<span className="pv1 ph2 bg-black white br2"><i className="fa fa-circle red"></i> <Trans>Live</Trans></span> : <span className="pv1 ph2 bg-black white br2"><Trans>Offline</Trans></span>
+														}
+														{new Boolean(+channel.live) ? <span className="pv1 ph2 bg-black white br2"><i className="fa fa-eye"></i> {channel.viewers}</span> : <></>}
 													</div>
 												</div>
 											</div>
