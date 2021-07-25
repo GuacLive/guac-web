94209bde64042abb0fe796983239ceb1c434a990
datagutt <datagutt@lekanger.no>
2021-07-25 20:48:02 +0200

    next.config.js: sentry cleanArtifacts

--- next.config.js
+++ next.config.js
@@ -54,7 +54,7 @@ module.exports = withTM(withOffline({
 					ignore: ['node_modules'],
 					stripPrefix: ['webpack://_N_E/'],
 					urlPrefix: '~/_next',
-					deleteAfterCompile: true,
+					cleanArtifacts: true,
 				})
 			);
 		}
