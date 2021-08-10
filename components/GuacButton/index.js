b20377e7f311dc1cb6790b603fe0dd0a1382afe0
datagutt <datagutt@lekanger.no>
2021-08-10 17:45:52 +0200

    GuacButton: Functional component

--- components/GuacButton/index.js
+++ components/GuacButton/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import Link from 'next/link'
 
 import classNames from 'classnames'
-export default class extends React.Component {
-
-	render() {
-		const isActive = this.props.isActive;
+export default function GuacButton(props){
+		const isActive = props.isActive;
 		// dib pv2 ph3 nowrap lh-solid pointer br2 ba b--transparent
-		const buttonClass = classNames(`link color-inherit dib pv2 ph3 nowrap lh-solid pointer br2 ba b--${this.props.border || this.props.color || 'transparent'} bg-${this.props.color || 'transparent'} guac-btn`)
-		return this.props.children ? (
-			<Link href={this.props.url || '#'}><a className={buttonClass} children={this.props.children} onClick={this.props.onClick} /></Link>
+		const buttonClass = classNames(`link color-inherit dib pv2 ph3 nowrap lh-solid pointer br2 ba b--${props.border || props.color || 'transparent'} bg-${props.color || 'transparent'} guac-btn`)
+		return props.children ? (
+			<Link href={props.url || '#'}><a className={buttonClass} children={props.children} onClick={props.onClick} /></Link>
 		) : null
 	}
-}
